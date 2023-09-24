9b517a3e0d376086fb562e831216145e14453b1f
Taiabur Rahman <taiaburbd@gmail.com>
2023-09-24 11:09:09 +0200

    cv update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div></div>
+  </template>
+  
+  <script>
+  import { collection, addDoc } from "firebase/firestore"
+  import db from './firebase/init.js'
+  
+  export default {
+    created() {
+      this.createUser()
+    },
+    methods: {
+      async createUser() {
+        const colRef = collection(db, 'users')
+        const dataObj = {
+          firstName: 'Jane',
+          lastName: 'Doe',
+          dob: '1991'
+        }
+  
+        const docRef = await addDoc(colRef, dataObj)
+        console.log('Document was created with ID:', docRef.id)
+      }
+    }
+  }
+  </script>
